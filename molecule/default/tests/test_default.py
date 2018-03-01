861619bb32680947032fc05aa283d119d75bff20
Pierre-Louis Bonicoli <pierre-louis.bonicoli@libregerbil.fr>
2018-03-01 17:01:38 +0100

    [CI] add test

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,14 +1,37 @@
+# -*- coding: utf-8 -*-
 import os
 
-import testinfra.utils.ansible_runner
+import yaml
 
-testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+import testinfra
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_lxc_container():
+
+    # testinfra uses a plain file for Ansible inventory: create it
+    updated_inventory = '%s.new.yml' % os.environ['MOLECULE_INVENTORY_FILE']
+
+    inventory = {
+        'all': {
+            'children': {
+                'lxc': {
+                    'hosts': {
+                        'testboot.lxc': {}
+                    }
+                },
+                'ungrouped': {}
+            }
+        },
+    }
+
+    with open(updated_inventory, 'w') as output:
+        output.write(yaml.dump(inventory))
+
+    host = testinfra.get_host('ansible://testboot.lxc?ansible_inventory=%s' %
+                              updated_inventory)
+
+    f = host.file('/srv/tests/canary')
 
     assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+    assert f.contains("inception")
+    assert not f.contains("not there")
